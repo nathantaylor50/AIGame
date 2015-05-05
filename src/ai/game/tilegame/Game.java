5259422a4cb6a471cf1d83d2de457447e25d9804
nathantaylor50 <nathantaylor50@msn.com>
2015-05-06 00:52:29 +0100

    added game class

--- src/ai/game/tilegame/Game.java
+++ src/ai/game/tilegame/Game.java
@@ -1,5 +1,17 @@
 package ai.game.tilegame;
 
+import ai.game.tilegame.display.Display;
+
 public class Game {
+	
+	private Display display;
+	
+	public int width, height;
+	
+	public Game(String title, int width, int height){
+		this.width = width;
+		this.height = height;
+		display = new Display(title, width, height);
+	}
 
 }
