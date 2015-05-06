81637052851770e2d86045c70c693232a1f21cc8
nathantaylor50 <nathantaylor50@msn.com>
2015-05-06 15:28:49 +0100

    Menu state

---
+++ src/ai/game/tilegame/states/MenuState.java
@@ -0,0 +1,22 @@
+package ai.game.tilegame.states;
+
+import java.awt.Graphics;
+
+public class MenuState extends State {
+
+	public MenuState(){
+		
+	}
+	
+	
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+}
