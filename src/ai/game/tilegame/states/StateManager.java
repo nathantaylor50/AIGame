ecb94d45a2dab69deff8aafd105abdac37ef9b6e
nathantaylor50 <nathantaylor50@msn.com>
2015-05-06 14:34:04 +0100

    state manager class

---
+++ src/ai/game/tilegame/states/StateManager.java
@@ -0,0 +1,15 @@
+package ai.game.tilegame.states;
+
+public class StateManager {
+
+	private static State currentState = null;
+	
+	public static void setState(State state){
+		currentState = state;
+	}
+	
+	public static State getState(){
+		return currentState;
+	}
+	
+}
