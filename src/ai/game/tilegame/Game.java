f9459f1b3c7d121359dd9377fe5a69d8c42fdd5d
nathantaylor50 <nathantaylor50@msn.com>
2015-05-06 12:48:59 +0100

    game loop optimized and fps counter added
    loop runs 60 times a second, so computer speed does not affect game
    speed

--- src/ai/game/tilegame/Game.java
+++ src/ai/game/tilegame/Game.java
@@ -30,8 +30,10 @@ public class Game implements Runnable {
 		Assets.init();
 	}
 	
+	int x = 0;
+	
 	private void update(){
-		
+		x += 1;
 	}
 	
 	private void render(){
@@ -45,7 +47,7 @@ public class Game implements Runnable {
 		//clear screen
 		g.clearRect(0, 0, width, height);
 		//draw to screen
-		g.drawImage(Assets.dirt, 10, 10, null);
+		g.drawImage(Assets.dirt, x, 10, null);
 		
 		
 		
@@ -66,11 +68,38 @@ public class Game implements Runnable {
 	//needs this method for 'implements runnable' to work
 	public void run(){
 		init();
+		//frames per second, 60 times per seond
+		int fps = 60;
+		//9 billion nano seconds(1 second) / fps
+		double timePerUpdate = 1000000000 / fps;
+		double delta = 0;
+		long now;
+		//returns amount of time in nano seconds player's computer is running at
+		long lastTime = System.nanoTime();
+		//fps counter
+		long timer = 0;
+		int updates = 0;
 		
 		//game loop
 		while(running){
-			update();
-			render();
+			//now = current time of computer in nano secs
+			now = System.nanoTime();
+			// delta += amount of time pasted / max amout of time to call update and render
+			delta += (now - lastTime) / timePerUpdate;
+			timer += now - lastTime;
+			//update last time
+			lastTime = now;	
+			if(delta >= 1){
+				update();
+				render();
+				updates++;
+				delta--;
+			}
+			if(timer >= 1000000000){
+				System.out.println("FPS: " + updates);
+				updates = 0;
+				timer = 0;
+			}
 		}
 		//stops thread
 		stop();
